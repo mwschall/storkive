fee4d0c7b74d6cb6bb9a7e523768c9f43f1a7705
Matthew Schallberger <mwschall@gmail.com>
2019-01-25 18:09:02 -0800

    Added a preliminary setup.py and version number.

---
+++ setup.py
@@ -0,0 +1,53 @@
+import os
+
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+about = {}
+with open(os.path.join('storkive', '__version__.py'), 'r', encoding='utf-8') as f:
+    exec(f.read(), about)
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url=about['__url__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    license=about['__license__'],
+
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Content Management System',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application,'
+    ],
+)
---
+++ storkive/__version__.py
@@ -0,0 +1,11 @@
+from django.utils.version import get_version
+
+VERSION = (0, 1, 0, 'alpha', 1)
+
+__title__ = 'Storkive'
+__version__ = get_version(VERSION)
+__description__ = 'Web app for managing story collections.'
+__url__ = 'https://github.com/mwschall/storkive'
+__author__ = 'Matthew Schallberger'
+__author_email__ = 'mwschall@gmail.com'
+__license__ = 'ISC License'
